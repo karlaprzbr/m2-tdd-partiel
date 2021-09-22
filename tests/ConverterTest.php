2228b4e555485a95777d28c94cb8fd682dd2a8a8
Karla Pérez <44835870+karlaprzbr@users.noreply.github.com>
2021-09-22 14:59:38 +0200

    écriture du test de la fonction decimalToRoman

---
+++ tests/ConverterTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class ConverterTest extends TestCase
+{
+    public function testDecimalToRoman($decimal): void
+    {
+        $this->assertSame("I", Converter::decimalToRoman(1));
+        $this->assertSame("IV", Converter::decimalToRoman(4));
+        $this->assertSame("IX", Converter::decimalToRoman(9));
+        $this->assertSame("L", Converter::decimalToRoman(50));
+        $this->assertSame("C", Converter::decimalToRoman(100));
+        $this->assertSame("D", Converter::decimalToRoman(500));
+        $this->assertSame("M", Converter::decimalToRoman(1000));
+    }
+}
